56c5b1a755fd40764cda207317098c7c23b2b66a
jinyoung-Kim0 <135565403+jinyoung-Kim0@users.noreply.github.com>
2023-06-13 18:01:06 +0900

    Feature : GameManager ObjectPool
    - 총알의 오브젝트풀을 GameManager에 구현

--- Assets/02 2.Script/GameManager.cs
+++ Assets/02 2.Script/GameManager.cs
@@ -6,7 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     [SerializeField] private Character character;
-
+    private Stack<Bullet> bulletPool;
+    private GameObject bulletParents;
     private void Awake()
     {
         if(instance == null)
@@ -17,10 +18,56 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        bulletPool = new Stack<Bullet>();
+        bulletParents = new GameObject("BulletPool");
+        bulletParents.transform.SetParent(transform);
     }
+
+
+    #region [Bullet] ObjectPool
+    /// <summary>
+    /// 
+    /// [Jinyoung Kim] 
+    /// 
+    /// Push the Bullet in the BulletPool
+    /// 
+    /// </summary>
+    /// <param name="_bullet"></param>
+    public void PushPoolBullet(Bullet _bullet)
+    {
+        _bullet.transform.SetParent(bulletParents.transform);
+        _bullet.gameObject.SetActive(false);
+        bulletPool.Push(_bullet);
+    }
+    /// <summary>
+    /// 
+    /// [Jinyoung Kim]
+    /// 
+    /// Take from BulletPool
+    /// 
+    /// </summary>
+    /// <param name="_bullet"></param>
+    public Bullet GetBullet()
+    {
+        Bullet bullet;
+        if (bulletPool.Count == 0)
+        {
+            Bullet prefab = Resources.Load<Bullet>("Bullet");
+            bullet = Instantiate(prefab);
+        }
+        else
+        {
+            bullet = bulletPool.Pop();
+            bullet.gameObject.SetActive(true);
+        }
+        bullet.transform.localScale = new Vector3(1f, 1f, 1f);
+        return bullet;
+    }
+    #endregion
 
     private void Start()
     {
-        InputManager.instance.SettingCharacter(character);
+        //InputManager.instance.SettingCharacter(character);
     }
 }
