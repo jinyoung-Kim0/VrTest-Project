f6f1ccbf3d60b0863ee3093792f4849c0ec50d39
jinyoung-Kim0 <135565403+jinyoung-Kim0@users.noreply.github.com>
2023-06-21 17:47:38 +0900

    Feature : MagazineTable
    탄창 배치 및 초기화

--- Assets/02.Script/GameManager.cs
+++ Assets/02.Script/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshPro scoreBoard;
 
     private Vector3 magazineBoxPos;
+    private List<Magazine> tableMagazine;
     private int score;
 
     private void Awake()
@@ -28,7 +29,49 @@ public class GameManager : MonoBehaviour
         score = 0;
         bulletPool = new ObjectPool<Bullet>(this.gameObject, "Bullet");
         targetPool = new ObjectPool<Target>(this.gameObject, "Target");
+        
         magazineBoxPos = new Vector3(6, 1.5f, 0);
+    }
+
+    private void TableMagazineInitialize()
+    {
+        tableMagazine = new List<Magazine>(9);
+        Vector3 preset = new Vector3(1f,0,0);
+        Magazine firstOne = Resources.Load<Magazine>("Magazine");
+
+        for (int i = 0; i < tableMagazine.Count; i++)
+        {
+            tableMagazine[i] = Instantiate(firstOne);
+        }
+        //firstOne.transform.position = new Vector3(5, 2, 1);
+        //for (int i = 1; i < tableMagazine.Count; i++)
+        //{
+        //    Vector3 pos = tableMagazine[i - 1].transform.position;
+        //    if(i % 4 == 0)
+        //    {
+        //        pos.z--;
+        //    }
+        //    else
+        //    {
+        //        pos.x++;
+        //    }
+        //    tableMagazine[i].transform.position = pos;
+        //}
+        
+    }
+
+    private void ResetTableMagazine()
+    {
+        Vector3 startPos = new Vector3(5, 2, 1);
+        int count = 0;
+        for (int i = 0; i < tableMagazine.Count; i++)
+        {
+            if(tableMagazine[i].IsFree())
+            {
+                tableMagazine[i].transform.position = startPos;
+                /// 여기서 부터 
+            }
+        }
     }
 
     private void Start()
@@ -46,6 +89,7 @@ public class GameManager : MonoBehaviour
     {
         ++score;
         scoreBoard.text = score.ToString();
+        targetPool.Push(_hitTarget);
     }
 
 }
